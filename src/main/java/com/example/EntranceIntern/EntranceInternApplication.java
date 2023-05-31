9c2eedce7da444e2abcff56db38ffd051b89a703
louis.le@kmail.vn <khoe.le.mall@gmail.com>
2023-05-31 22:29:11 +0700

    #imp : update git ignore

---
+++ src/main/java/com/example/EntranceIntern/EntranceInternApplication.java
@@ -0,0 +1,13 @@
+package com.example.EntranceIntern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntranceInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntranceInternApplication.class, args);
+	}
+
+}
